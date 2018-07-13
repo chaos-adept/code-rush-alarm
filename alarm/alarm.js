db8b49fa40e873bf891a28c32116a0e0ef7664f0
va1tra <time_traveller@mail.ru>
2018-07-13 18:46:42 +0300

    add fixed notification

--- alarm/alarm.js
+++ alarm/alarm.js
@@ -1,15 +1,23 @@
 const notifier = require('node-notifier');
 
 function notify(state) {
-    console.log(state);
-
-    notifier.notify({
-        title: 'Alarm',
-        message: 'We have failed tests on master.',
-        sound: true,
-        wait: false,
-        type: 'error'
-    });
+    if (state) {
+        notifier.notify({
+            title: 'Alarm',
+            message: 'We have failed tests on master.',
+            sound: true,
+            wait: false,
+            type: 'error'
+        });
+    } else {
+        notifier.notify({
+            title: 'Keep calm',
+            message: 'Tests on master were fixed.',
+            sound: true,
+            wait: false,
+            type: 'info'
+        });
+    }
 }
 
 module.exports = notify;
