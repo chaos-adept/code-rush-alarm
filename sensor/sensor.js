b214a2fc8c218c0ecc9fbb3982e18e47bf51228a
Rykovanov <drykovanov@wiley.com>
2018-07-13 18:53:16 +0300

    [+] sensor

--- sensor/sensor.js
+++ sensor/sensor.js
@@ -1,5 +1,48 @@
+const http = require('http');
 const EventEmitter = require('events');
 
-const sensor = new EventEmitter();
+function getBuildStatus(callback) {
+    const request = http.request(
+        {
+            'hostname': '192.168.108.166',
+            'port': 8080,
+            'auth': 'rakibov:Sg8MNKSa',
+            'path': '/job/wpp-as/job/as-portal/job/health-checks/job/as-portal-master-mock-health-check/lastBuild/api/json'
+        },
+        function (resp) {
+            resp.setEncoding('utf8');
 
-module.exports = sensor;
+            let data = '';
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            resp.on('end', () => {
+                callback(JSON.parse(data));
+            });
+        }
+    );
+    request.end();
+};
+
+
+const alarmer = new EventEmitter();
+
+let prevState = 'SUCCESS';
+setInterval(() => {
+    getBuildStatus((res) => {
+        if (res.result != prevState) {
+            prevState = res.result;
+
+            if (prevState == 'SUCCESS') {
+                alarmer.emit('fixed');
+            } else {
+                alarmer.emit('alarm');
+            }
+        }
+
+        console.log(`Current: ${res.result}`);
+    })
+}, 60000);
+
+module.exports = alarmer;
