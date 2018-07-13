3cbf571d9aed095255181f1aedc4ca011105d068
va1tra <time_traveller@mail.ru>
2018-07-13 18:37:30 +0300

    add simple notification

--- alarm/alarm.js
+++ alarm/alarm.js
@@ -1,5 +1,15 @@
+const notifier = require('node-notifier');
+
 function notify(state) {
-    console.log(state)
+    console.log(state);
+
+    notifier.notify({
+        title: 'Alarm',
+        message: 'We have failed tests on master.',
+        sound: true,
+        wait: false,
+        type: 'error'
+    });
 }
 
 module.exports = notify;
